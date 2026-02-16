062bffa664a138b6588a9dfabde3e6d240063045
AyoubDjerboua <ayoub.djerboua26@gmail.com>
2026-02-16 09:39:19 +0100

    Affichage bonnes images correspondantes à l'arme

--- script.js
+++ script.js
@@ -2,113 +2,113 @@ const weaponsByType = [
     {
         type: "Grande épée",
         weapons: [
-            { name: "Wyvern Ignition", price: 12800 },
-            { name: "Nergal Judicator", price: 11600 },
-            { name: "Rathalos Gladius", price: 9800 }
+            { name: "Abaddonian Krake", price: 12200, image: "https://static.wikia.nocookie.net/monsterhunter/images/a/af/MHWilds-Great_Sword_Render_010.png/revision/latest/scale-to-width-down/320?cb=20250321202324" },
+            { name: "Akanesasu", price: 8900, image: "https://static.wikia.nocookie.net/monsterhunter/images/0/08/MHWilds-Great_Sword_Render_029.png/revision/latest/scale-to-width-down/320?cb=20250405031030" },
+            { name: "Albirath Wing I", price: 8300, image: "https://static.wikia.nocookie.net/monsterhunter/images/a/aa/MHWilds-Great_Sword_Render_002.png/revision/latest/scale-to-width-down/320?cb=20250321193019" }
         ]
     },
     {
         type: "Épée longue",
         weapons: [
-            { name: "Divine Slasher", price: 12100 },
-            { name: "Reaver Calamity", price: 11300 },
-            { name: "Tigrine Need", price: 9700 }
+            { name: "Artian Saber I", price: 12900, image: "https://static.wikia.nocookie.net/monsterhunter/images/9/95/MHWilds-Long_Sword_Render_025.png/revision/latest/scale-to-width-down/320?cb=20250330012359" },
+            { name: "Barina Flamberge I", price: 9900, image: "https://static.wikia.nocookie.net/monsterhunter/images/3/37/MHWilds-Long_Sword_Render_017.png/revision/latest/scale-to-width-down/314?cb=20250326204855" },
+            { name: "Blanberge I", price: 9700, image: "https://static.wikia.nocookie.net/monsterhunter/images/4/4f/MHWilds-Long_Sword_Render_024.png/revision/latest/scale-to-width-down/314?cb=20250326212501" }
         ]
     },
     {
         type: "Épée et bouclier",
         weapons: [
-            { name: "Corona", price: 9100 },
-            { name: "Rogue Wave", price: 8600 },
-            { name: "Odium", price: 8900 }
+            { name: "Ajara Saber I", price: 9600, image: "https://static.wikia.nocookie.net/monsterhunter/images/7/78/MHWilds-Sword_and_Shield_Render_015.png/revision/latest/scale-to-width-down/317?cb=20250331004637" },
+            { name: "Albirath Edge I", price: 9000, image: "https://static.wikia.nocookie.net/monsterhunter/images/0/01/MHWilds-Sword_and_Shield_Render_002.png/revision/latest/scale-to-width-down/320?cb=20250330024255" },
+            { name: "Artian Sword I", price: 12900, image: "https://static.wikia.nocookie.net/monsterhunter/images/f/f5/MHWilds-Sword_and_Shield_Render_027.png/revision/latest/scale-to-width-down/320?cb=20250331022252" }
         ]
     },
     {
         type: "Doubles lames",
         weapons: [
-            { name: "Fire and Ice", price: 10200 },
-            { name: "Decimation Claws", price: 10500 },
-            { name: "Jyura Hatchets", price: 8400 }
+            { name: "Ajara Twin Edges I", price: 8800, image: "https://static.wikia.nocookie.net/monsterhunter/images/0/0f/MHWilds-Dual_Blades_Render_012.png/revision/latest/scale-to-width-down/317?cb=20250402194600" },
+            { name: "Artian Edges I", price: 12500, image: "https://static.wikia.nocookie.net/monsterhunter/images/8/8a/MHWilds-Dual_Blades_Render_027.png/revision/latest/scale-to-width-down/320?cb=20250404003708" },
+            { name: "Barina Faucilles I", price: 12900, image: "https://static.wikia.nocookie.net/monsterhunter/images/a/ad/MHWilds-Dual_Blades_Render_018.png/revision/latest/scale-to-width-down/320?cb=20250402203859" }
         ]
     },
     {
         type: "Marteau",
         weapons: [
-            { name: "Diablos Shatterer", price: 11900 },
-            { name: "Empress Mace Blaze", price: 11200 },
-            { name: "Bone Bludgeon", price: 7800 }
+            { name: "Abaddonian Pulpo", price: 11600, image: "https://static.wikia.nocookie.net/monsterhunter/images/4/4c/MHWilds-Hammer_Render_012.png/revision/latest/scale-to-width-down/320?cb=20250407014246" },
+            { name: "Albirath Bludgeon I", price: 8700, image: "https://static.wikia.nocookie.net/monsterhunter/images/e/ed/MHWilds-Hammer_Render_002.png/revision/latest/scale-to-width-down/320?cb=20250407003038" },
+            { name: "Argyl Hammer", price: 11900, image: "https://static.wikia.nocookie.net/monsterhunter/images/3/30/MHWilds-Hammer_Render_028.png/revision/latest/scale-to-width-down/320?cb=20250701021201" }
         ]
     },
     {
         type: "Cor de chasse",
         weapons: [
-            { name: "Deep Vero", price: 10600 },
-            { name: "Teostra's Orphee", price: 11500 },
-            { name: "Queen's Flute", price: 9300 }
+            { name: "Ajara Reverberator I", price: 10900, image: "https://static.wikia.nocookie.net/monsterhunter/images/9/98/MHWilds-Hunting_Horn_Render_005.png/revision/latest/scale-to-width-down/320?cb=20250409011607" },
+            { name: "Albirath Feroce I", price: 8400, image: "https://static.wikia.nocookie.net/monsterhunter/images/9/90/MHWilds-Hunting_Horn_Render_002.png/revision/latest/scale-to-width-down/320?cb=20250409011601" },
+            { name: "Allysyl Muse", price: 8300, image: "https://static.wikia.nocookie.net/monsterhunter/images/e/ea/MHWilds-Hunting_Horn_Render_029.png/revision/latest/scale-to-width-down/320?cb=20250701022009" }
         ]
     },
     {
         type: "Lance",
         weapons: [
-            { name: "Fiendish Tower", price: 11100 },
-            { name: "Babel Spear", price: 9900 },
-            { name: "Nergal Impaler", price: 10800 }
+            { name: "Abaddonian Osminog", price: 8700, image: "https://static.wikia.nocookie.net/monsterhunter/images/4/46/MHWilds-Lance_Render_008.png/revision/latest/scale-to-width-down/314?cb=20250416151352" },
+            { name: "Acrus Lance I", price: 9500, image: "https://static.wikia.nocookie.net/monsterhunter/images/3/3f/MHWilds-Lance_Render_031.png/revision/latest/scale-to-width-down/320?cb=20250701023826" },
+            { name: "Aether Pike", price: 9600, image: "https://static.wikia.nocookie.net/monsterhunter/images/1/10/MHWilds-Lance_Render_032.png/revision/latest/scale-to-width-down/320?cb=20251220183056" }
         ]
     },
     {
         type: "Lance-canon",
         weapons: [
-            { name: "Royal Burst", price: 11400 },
-            { name: "Earthshaker Magda", price: 11800 },
-            { name: "Steel Assault", price: 9400 }
+            { name: "Ajara Gunpike I", price: 11400, image: "https://static.wikia.nocookie.net/monsterhunter/images/a/a8/MHWilds-Gunlance_Render_005.png/revision/latest/scale-to-width-down/314?cb=20250417004440" },
+            { name: "Albirath Rook I", price: 12000, image: "https://static.wikia.nocookie.net/monsterhunter/images/1/12/MHWilds-Gunlance_Render_002.png/revision/latest/scale-to-width-down/320?cb=20250417004433" },
+            { name: "Argenesis", price: 8300, image: "https://static.wikia.nocookie.net/monsterhunter/images/d/da/MHWilds-Gunlance_Render_022.png/revision/latest/scale-to-width-down/314?cb=20250417004833" }
         ]
     },
     {
         type: "Morpho-hache",
         weapons: [
-            { name: "Power Smasher", price: 10900 },
-            { name: "Axe of Demons", price: 9800 },
-            { name: "Jagras Raider", price: 8200 }
+            { name: "Abaddonian Zhangyu", price: 11700, image: "https://static.wikia.nocookie.net/monsterhunter/images/d/d0/MHWilds-Switch_Axe_Render_007.png/revision/latest/scale-to-width-down/320?cb=20250420001034" },
+            { name: "Aksa Switch I", price: 9400, image: "https://static.wikia.nocookie.net/monsterhunter/images/5/5d/MHWilds-Switch_Axe_Render_026.png/revision/latest/scale-to-width-down/320?cb=20250702010312" },
+            { name: "Albirath Blaze Splitter", price: 12700, image: "https://static.wikia.nocookie.net/monsterhunter/images/2/2e/MHWilds-Switch_Axe_Render_002.png/revision/latest/scale-to-width-down/320?cb=20250420000814" }
         ]
     },
     {
         type: "Volto-hache",
         weapons: [
-            { name: "Dear Lutemia", price: 12500 },
-            { name: "Tyrannis II", price: 11700 },
-            { name: "Kadachi Kaina", price: 9600 }
+            { name: "Abaddonian Pweza", price: 12300, image: "https://static.wikia.nocookie.net/monsterhunter/images/8/8f/MHWilds-Charge_Blade_Render_013.png/revision/latest/scale-to-width-down/320?cb=20250420203629" },
+            { name: "Abiding Gawain", price: 12600, image: "https://static.wikia.nocookie.net/monsterhunter/images/f/f0/MHWilds-Charge_Blade_Render_010.png/revision/latest/scale-to-width-down/320?cb=20250420203542" },
+            { name: "Albirath Blade I", price: 11600, image: "https://static.wikia.nocookie.net/monsterhunter/images/9/98/MHWilds-Charge_Blade_Render_002.png/revision/latest/scale-to-width-down/320?cb=20250420203436" }
         ]
     },
     {
         type: "Insectoglaive",
         weapons: [
-            { name: "Catastrophe's Light", price: 11200 },
-            { name: "True Gae Bolg", price: 12600 },
-            { name: "Blooming Glaive", price: 9200 }
+            { name: "Aerial Glaive", price: 6800, image: "https://static.wikia.nocookie.net/monsterhunter/images/2/2f/MHWilds-Insect_Glaive_Render_013.png/revision/latest/scale-to-width-down/320?cb=20250422010244" },
+            { name: "Ajara Trident I", price: 9600, image: "https://static.wikia.nocookie.net/monsterhunter/images/0/0d/MHWilds-Insect_Glaive_Render_014.png/revision/latest/scale-to-width-down/320?cb=20250422010246" },
+            { name: "Alabaster Rathmaul", price: 11000, image: "https://static.wikia.nocookie.net/monsterhunter/images/7/77/MHWilds-Insect_Glaive_Render_002.png/revision/latest/scale-to-width-down/320?cb=20250422010047" }
         ]
     },
     {
         type: "Fusarbalète léger",
         weapons: [
-            { name: "Karma", price: 9900 },
-            { name: "Blitz Support", price: 9400 },
-            { name: "Luna Styx", price: 10800 }
+            { name: "Abaddonian Chobotnice", price: 11900, image: "https://static.wikia.nocookie.net/monsterhunter/images/5/55/MHWilds-Light_Bowgun_Render_022.png/revision/latest/scale-to-width-down/320?cb=20250423220810" },
+            { name: "Ajara Shellgun I", price: 9900, image: "https://static.wikia.nocookie.net/monsterhunter/images/a/a0/MHWilds-Light_Bowgun_Render_019.png/revision/latest/scale-to-width-down/320?cb=20250423220804" },
+            { name: "Ajara-Angaja", price: 14200, image: "https://static.wikia.nocookie.net/monsterhunter/images/6/6d/MHWilds-Light_Bowgun_Render_020.png/revision/latest/scale-to-width-down/320?cb=20250423220806" }
         ]
     },
     {
         type: "Fusarbalète lourd",
         weapons: [
-            { name: "Dark Devourer", price: 12200 },
-            { name: "Magda Gemitus", price: 11000 },
-            { name: "Legia Shattercryst", price: 10100 }
+            { name: "Abaddonian Muneo", price: 13000, image: "https://static.wikia.nocookie.net/monsterhunter/images/3/3a/MHWilds-Heavy_Bowgun_Render_024.png/revision/latest/scale-to-width-down/320?cb=20250427022431" },
+            { name: "Ableben Abzug", price: 9200, image: "https://static.wikia.nocookie.net/monsterhunter/images/5/51/MHWilds-Heavy_Bowgun_Render_036.png/revision/latest/scale-to-width-down/320?cb=20250427223832" },
+            { name: "Aeternal Palamedes", price: 12600, image: "https://static.wikia.nocookie.net/monsterhunter/images/9/93/MHWilds-Heavy_Bowgun_Render_014.png/revision/latest/scale-to-width-down/320?cb=20250427021443" }
         ]
     },
     {
         type: "Arc",
         weapons: [
-            { name: "Anja Arch III", price: 10300 },
-            { name: "Doom's Shaft", price: 11900 },
-            { name: "Flying Kadachi Strikebow", price: 9700 }
+            { name: "Albirath Bow I", price: 10900, image: "https://static.wikia.nocookie.net/monsterhunter/images/b/b4/MHWilds-Bow_Render_002.png/revision/latest/scale-to-width-down/320?cb=20250427234747" },
+            { name: "Angelbein", price: 10300, image: "https://static.wikia.nocookie.net/monsterhunter/images/1/15/MHWilds-Bow_Render_024.png/revision/latest/scale-to-width-down/320?cb=20250428005016" },
+            { name: "Blan Sharanga I", price: 9900, image: "https://static.wikia.nocookie.net/monsterhunter/images/9/9b/MHWilds-Bow_Render_023.png/revision/latest/scale-to-width-down/320?cb=20250428004934" }
         ]
     }
 ];
@@ -120,25 +120,8 @@ const clearCartButton = document.getElementById("clear-cart");
 
 const cart = [];
 
-const weaponTypeImages = {
-    "Grande épée": "https://static.wikia.nocookie.net/monsterhunter/images/1/15/MH4-Great_Sword_Equipment_Render_001.png/revision/latest/scale-to-width-down/200?cb=20140122011522",
-    "Épée longue": "https://static.wikia.nocookie.net/monsterhunter/images/f/fc/MH4-Long_Sword_Equipment_Render_001.png/revision/latest/scale-to-width-down/200?cb=20140122011520",
-    "Épée et bouclier": "https://static.wikia.nocookie.net/monsterhunter/images/b/bf/MH4-Sword_and_Shield_Equipment_Render_001.png/revision/latest/scale-to-width-down/200?cb=20140122011519",
-    "Doubles lames": "https://static.wikia.nocookie.net/monsterhunter/images/6/6f/MH4-Dual_Blades_Equipment_Render_001.png/revision/latest/scale-to-width-down/200?cb=20140615225729",
-    "Marteau": "https://static.wikia.nocookie.net/monsterhunter/images/3/32/MH4-Hammer_Equipment_Render_001.png/revision/latest/scale-to-width-down/200?cb=20140122011521",
-    "Cor de chasse": "https://static.wikia.nocookie.net/monsterhunter/images/b/b1/MH4-Hunting_Horn_Equipment_Render_001.png/revision/latest/scale-to-width-down/200?cb=20140122011521",
-    "Lance": "https://static.wikia.nocookie.net/monsterhunter/images/5/54/MH4-Lance_Equipment_Render_001.png/revision/latest/scale-to-width-down/200?cb=20140122011520",
-    "Lance-canon": "https://static.wikia.nocookie.net/monsterhunter/images/d/d7/MH4-Gunlance_Equipment_Render_001.png/revision/latest/scale-to-width-down/125?cb=20140122011522",
-    "Morpho-hache": "https://static.wikia.nocookie.net/monsterhunter/images/c/c8/MH4-Switch_Axe_Equipment_Render_001.png/revision/latest/scale-to-width-down/182?cb=20140626032107",
-    "Volto-hache": "https://static.wikia.nocookie.net/monsterhunter/images/5/56/MH4-Charge_Blade_Equipment_Render_001.png/revision/latest/scale-to-width-down/175?cb=20140610185223",
-    "Insectoglaive": "https://static.wikia.nocookie.net/monsterhunter/images/9/9b/MH4-Insect_Glaive_Equipment_Render_001.png/revision/latest/scale-to-width-down/182?cb=20140610185945",
-    "Fusarbalète léger": "https://static.wikia.nocookie.net/monsterhunter/images/3/36/MH4-Light_Bowgun_Equipment_Render_001.png/revision/latest/scale-to-width-down/200?cb=20140122011520",
-    "Fusarbalète lourd": "https://static.wikia.nocookie.net/monsterhunter/images/b/bc/MH4-Heavy_Bowgun_Equipment_Render_001.png/revision/latest/scale-to-width-down/200?cb=20140122011521",
-    "Arc": "https://static.wikia.nocookie.net/monsterhunter/images/5/5a/MH4-Bow_Equipment_Render_001.png/revision/latest/scale-to-width-down/200?cb=20140610184349"
-};
-
-function imageUrl(type, label) {
-    return weaponTypeImages[type] || `https://placehold.co/300x180/1f2230/eceef8?text=${encodeURIComponent(label)}`;
+function imageUrl(image, label) {
+    return image || `https://placehold.co/300x180/1f2230/eceef8?text=${encodeURIComponent(label)}`;
 }
 
 function renderCatalogue() {
@@ -158,7 +141,7 @@ function renderCatalogue() {
             card.className = "weapon-card";
 
             const img = document.createElement("img");
-            img.src = imageUrl(category.type, weapon.name);
+            img.src = imageUrl(weapon.image, weapon.name);
             img.alt = weapon.name;
 
             const weaponName = document.createElement("h4");
